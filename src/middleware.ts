4abc9038b698d5f8c7456b5c407d1c137279e23e
Muhammad Yahya Zahid <myahyazahid11@gmail.com>
2026-09-21 11:44:23 +0700

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
+// Lindungi semua route kecuali login, api auth, dan public folder
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - login (login page)
+     * - api/auth (auth api routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, favicon.png (favicon files)
+     */
+    "/((?!login|api/auth|_next/static|_next/image|favicon.ico|favicon.png).*)",
+  ],
+};
